19fa698bdabde059981de19ed8367eaee9c20f06
huanghongqing <eclipseiscool@gmail.com>
2020-11-28 15:33:26 +0800

    hook useContext

--- gadue_demo/ReactHooks1/hooksd1/src/Example4.js
+++ gadue_demo/ReactHooks1/hooksd1/src/Example4.js
@@ -1,2 +1,32 @@
-import React, { Component,useContext } from 'react';
+import React, { Component,useState,createContext,useContext }from 'react';
+import {BrowserRouter as Router ,Route,Link} from 'react-router-dom'
 
+const CountContext=createContext()
+
+function Example(){
+    const [count,setCount]=useState(0)  //数据解构
+
+    return (
+            <div>
+                <p>click here {count} times.</p>
+                <button onClick={()=>{
+                    setCount(count+1)
+                }}>
+                    click me.
+                </button>
+                {/* 父组件传值 */}
+                <CountContext.Provider value={count}>  
+                    <Counter></Counter>
+                </CountContext.Provider>
+            </div>
+         );       
+    
+
+}
+
+function Counter(){
+    let count= useContext(CountContext)  //子组件接收到值。
+    return (<h2>{count}</h2>)
+}
+ 
+export default Example;
--- gadue_demo/ReactHooks1/hooksd1/src/index.js
+++ gadue_demo/ReactHooks1/hooksd1/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import Example from './Example'
 import Example2 from './Example2'
 import Example3 from './Example3'
+import Example4 from './Example4'
 
 
 ReactDOM.render(
-    <Example />,
+    <Example4 />,
   document.getElementById('root')
 );
 
