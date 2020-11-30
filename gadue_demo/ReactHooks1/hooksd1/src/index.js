7e6ed73a6c28b9e262216a5cb7460ba5504c0ea9
huanghongqing <huanghq@eaccessy.com>
2020-11-30 23:10:26 +0800

    comment for hook index.js

--- gadue_demo/ReactHooks1/hooksd1/src/index.js
+++ gadue_demo/ReactHooks1/hooksd1/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example'
-import Example2 from './Example2'
-import Example3 from './Example3'
-import Example4 from './Example4'
-import ReducerD from './Example5'
-import Example6 from './cssShow/Example6'
-import Example7 from './Example7'
-import Example8 from './Example8'
+// import Example from './Example'
+// import Example2 from './Example2'
+// import Example3 from './Example3'
+// import Example4 from './Example4'
+// import ReducerD from './Example5'
+// import Example6 from './cssShow/Example6'
+// import Example7 from './Example7'
+// import Example8 from './Example8'
 import Example9 from './Example9'
 
 ReactDOM.render(
