74b71ea0edf46eb651fca13b1b68827317d8f874
huanghongqing <huanghq@eaccessy.com>
2020-12-03 00:42:23 +0800

    next_js2 axios and promise call back and catch error handle .

--- next_js2/pages/Device.js
+++ next_js2/pages/Device.js
@@ -1,14 +1,44 @@
-import {withRouter} from 'next/router'
+/* eslint-disable react/prop-types */
+import { withRouter} from 'next/router'
 import Link from 'next/link'
+import axios from 'axios'
 
-const Device =({router}) =>{
-    return(
+const Device = ({router,list})=>{
+    return (
         <>
-            <div>
-                {router.query.name}  ,
-                {router.query.numbers}
-            </div>
+            <div>{router.query.name},来为我们服务了 .<br/>{list}</div>
+            <Link href="/"><a>返回首页</a></Link>
         </>
     )
 }
+
+Device.getInitialProps = async ()=>{
+    const promise =new Promise((resolve,reject)=>{
+            axios('https://www.easy-mock.com/mock/5d25ef5ac61e164478f7d905/device/list').then(//这里直接用axios的then成功方法替代了promise的成功回调
+                (res)=>{
+                    console.log('远程数据结果：',res)
+                    resolve(res.data)  
+                    //resolve表示promis 已经定型，成功就把res.data 赋值给promise对象。因此相当于成功后的返回值，
+                    //而这个静态函数貌似是getInitialProps初始化props，因此就会让list获得这个值。失败用reject
+                }
+            ).catch(
+                (error)=>{
+                    console.log("error="+error)
+                    reject(error)
+                }
+            )
+    }).then(//resolve后传递过来的参数，在promise的成功回调里处理。
+        (list)=>{
+            console.log("list="+list)
+            return list;
+        }
+    ).catch(//promise的失败回调，这里也必须return一个值，不然会报undefined的错。
+        (error)=>{
+            console.log("error from promise ="+error)
+            return ({error})
+        }
+    )
+    return await promise
+}
+
 export default withRouter(Device)
--- next_js2/pages/index.js
+++ next_js2/pages/index.js
@@ -3,23 +3,23 @@ import Link from 'next/link'
 import Router from 'next/router'
 const Home=()=>{
 
-    Router.events.on('routeChangeStart',(...args)=>{
-        console.log("routhe change start.",...args)
-    })
-    Router.events.on('routeChangeComplete',(...args)=>{
-        console.log("routhe change complete.",...args)
-    })
+    // Router.events.on('routeChangeStart',(...args)=>{
+    //     console.log("routhe change start.",...args)
+    // })
+    // Router.events.on('routeChangeComplete',(...args)=>{
+    //     console.log("routhe change complete.",...args)
+    // })
 
-    Router.events.on('beforeHistoryChange',(...args)=>{
-        console.log("before History Change.",...args)
-    })
+    // Router.events.on('beforeHistoryChange',(...args)=>{
+    //     console.log("before History Change.",...args)
+    // })
 
-    Router.events.on('hashChangeStart',(...args)=>{
-        console.log("hash Change Start.",...args)  //hash 连接<Link href="#tag"><a>tag</a></Link>  ，scroll to某个标签
-        // #的含义，hash连接，例如 http://example.com/index.html#user;
-        // 就代表网页index.html的print位置。浏览器读取这个URL后，会自动将print位置滚动至可视区域。
-        // 为网页位置指定标识符，有两个方法。一是使用锚点，比如，二是使用id属性，html里针对#值得变化，增加了onhashchange事件
-    })
+    // Router.events.on('hashChangeStart',(...args)=>{
+    //     console.log("hash Change Start.",...args)  //hash 连接<Link href="#tag"><a>tag</a></Link>  ，scroll to某个标签
+    //     // #的含义，hash连接，例如 http://example.com/index.html#user;
+    //     // 就代表网页index.html的print位置。浏览器读取这个URL后，会自动将print位置滚动至可视区域。
+    //     // 为网页位置指定标识符，有两个方法。一是使用锚点，比如，二是使用id属性，html里针对#值得变化，增加了onhashchange事件
+    // })
     function goB(){
         //Router.push("/Bpage")
         //Router.push("/Device?name='T-80U'")
