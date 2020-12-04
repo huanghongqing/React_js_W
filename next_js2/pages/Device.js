07b8de6e049e2313ddf4b6b2a1680488b83cef2d
huanghongqing <huanghq@eaccessy.com>
2020-12-04 21:34:27 +0800

    next_js2 head for seo .

--- next_js2/pages/Device.js
+++ next_js2/pages/Device.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
 import { withRouter} from 'next/router'
 import Link from 'next/link'
 import axios from 'axios'
---
+++ next_js2/pages/header.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import head from 'next/head'
+
+function Header(){
+    return(
+        <Fragment>
+            <hea>
+                <title>Device list site</title>
+                <meta charSet="utf-8"></meta>
+            </hea>
+            <div>Header.js</div>
+        </Fragment>
+    )
+}
+
+export default Header
