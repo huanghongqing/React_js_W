030a4c67bb3c4522617b389d0b201c7bcb7feee8
huanghongqing <eclipseiscool@gmail.com>
2020-12-07 16:53:32 +0800

    react_hook  package  useMethods

---
+++ react_hook_lib_a/src/useMethods.js
@@ -0,0 +1,20 @@
+import {useState,useMemo} from 'react'
+
+export const useMethods = (initialValue, methods) => {
+    const [value, setValue] = useState(initialValue);
+    const boundMethods = useMemo(
+        () => Object.entries(methods).reduce(
+            (methods, [name, fn]) => {
+                console.log(methods)
+                const method = (...args) => {
+                    setValue(value => fn(value, ...args));
+                };
+                methods[name] = method;
+                return methods;
+            },
+            {}
+        ),
+        [methods]
+    );
+    return [value, boundMethods];
+};
