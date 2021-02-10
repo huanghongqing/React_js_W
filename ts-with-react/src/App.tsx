6bd11e4f84bb57c22af0bf0ac62eb9fed224afa0
huanghongqing <eclipseiscool@gmail.com>
2021-02-10 14:02:27 +0800

    ts-with-react useContext and useRef

--- ts-with-react/src/App.tsx
+++ ts-with-react/src/App.tsx
@@ -2,24 +2,36 @@ import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import useURLoader from './componenets/useURLoader'
+import LikeButton from './componenets/LikeButton';
 interface IShowResult{
   message:string,
   status:string,
 }
+interface IThemeProps {
+  [key:string]:{color:string;background:string}  //[key:string]=>索引签名， 可以用IThemeProps[key]={...}这样的格式
+}
+const themes:IThemeProps ={
+  'light':{color:'#000',background:'#eee'},
+  'dark':{color:'#fff',background:'#222'},
+}
+
+export const ThemeContext=React.createContext(themes.light) //记得导出一下
 function App() {
   const [ show,setShow]=useState(true)
   const [data,loading] =useURLoader('https://dog.ceo/api/breeds/image/random',[show])
   const dogResult=data as IShowResult
   return (
-    <div className="App">
+  <div className="App">
+    <ThemeContext.Provider value={themes.dark}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <button onClick={()=>{setShow(!show)}}>Toggle Tracker </button>
         </p>
-        <div>
+        <LikeButton />
+        {/* <div>
         { loading ? <p>loading dog image.</p> : <img src={dogResult && dogResult.message}></img>}
-        </div>
+        </div> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -29,7 +41,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </ThemeContext.Provider>
+  </div>
   );
 }
 
--- ts-with-react/src/componenets/LikeButton.tsx
+++ ts-with-react/src/componenets/LikeButton.tsx
@@ -1,8 +1,31 @@
-import React,{ Fragment, useState,useEffect } from 'react'
+import React,{ Fragment, useState,useEffect,useRef,useContext  } from 'react'
+import {ThemeContext} from '../App'
 
 const LikeButton:React.FC = ()=>{
     const [like,setLike]=useState(0)
     const [on,setOn]=useState(true)
+    const likeRef=useRef(0) //所有的
+    const didMountRef=useRef(false)
+    const domRef=useRef<HTMLInputElement>(null) //用来访问Dom节点，获取节点的最新值
+    const theme=useContext(ThemeContext) 
+    console.log(theme)
+    const style={
+        color:theme.color,
+        background:theme.background,
+    }
+    useEffect(
+        ()=>{
+            // if(didMountRef.current){
+            //     console.log("components updated.")
+            // }else{
+            //     didMountRef.current=true;
+            // }
+            if(domRef && domRef.current){
+                domRef.current.focus()
+                domRef.current.value='link hi'
+            }
+        }
+    )
     useEffect(
         ()=>{
             console.log('effect executed.')
@@ -10,14 +33,18 @@ const LikeButton:React.FC = ()=>{
         },
         [like,on]
     )
+    function handleAlertClick(){
+        setTimeout(()=>{//这里形成了一个闭包
+            alert('you clicked.'+likeRef.current) //用useRef的likeRef.current，每次都能取到最新的值，不是闭包时封装进去的值,修改ref值不会引发重新渲染
+        },3000)
+    }
     return (
         <Fragment>
-            <button onClick={()=>{setLike(like+1)}}>
+            <button style={style} onClick={()=>{setLike(like+1);likeRef.current=like;}}>
                 {like} 👍️
             </button>
-            <button onClick={()=>{setOn(!on)}}>
-                {on?'ON':'OFF'}
-            </button>
+            <button onClick={handleAlertClick}>Alert.</button>
+            <input type='text' ref={domRef} />
         </Fragment>
     )
 }
